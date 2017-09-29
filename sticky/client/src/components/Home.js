e5acd9ce464afd357cc0dc526e0d1d8cc3f15255
Kris Gray <kris.m.gray@gmail.com>
2017-09-29 04:35:00 -0600

    created full repack app base for sticky notes

---
+++ sticky/client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Header } from 'semantic-ui-react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>Sticky Notes</div>
+    );
+  }
+}
+
+export default Home;
